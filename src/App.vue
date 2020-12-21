c925ec7de256530357361a3360726e53703c083b
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-12-21 12:28:33 +0100

    colored background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :style="{backgroundColor: randomColor()}">
     <b-container fluid>
       <b-row>
         <h1 class="logo">Hab ich saufi gehört</h1>
@@ -23,13 +23,65 @@ export default {
   components: {
     Decider,
     Task
-  }
+  },
+  data () {
+    return {
+      bright: [
+        {hexcode: '#54478C'},
+        {hexcode: '#2C699A'},
+        {hexcode: '#048BA8'},
+        {hexcode: '#0DB39E'},
+        {hexcode: '#16DB93'},
+        {hexcode: '#83E377'},
+        {hexcode: '#B9E769'},
+        {hexcode: '#EFEA5A'},
+        {hexcode: '#F1C453'},
+        {hexcode: '#F29E4C'}
+      ],
+      smallBright: [
+        {hexcode: '#1A535C'},
+        {hexcode: '#4ECDC4'},
+        {hexcode: '#F7FFF7'},
+        {hexcode: '#FF6B6B'},
+        {hexcode: '#FFE66D'}
+      ],
+      pastel: [
+        {hexcode: '#FFADAD'},
+        {hexcode: '#FFD6A5'},
+        {hexcode: '#FDFFB6'},
+        {hexcode: '#CAFFBF'},
+        {hexcode: '#9BF6FF'},
+        {hexcode: '#A0C4FF'},
+        {hexcode: '#BDB2FF'},
+        {hexcode: '#FFC6FF'},
+        {hexcode: '#FFFFFC'}
+      ]
+    }
+  },
+  methods: {
+    randomColor() {
+      const length = this.pastel.length
+      const color = Math.floor(Math.random() * (length - 0 + 1)) + 0
+      return this.pastel[color].hexcode
+    }
+  },
 }
 </script>
 
-<style scoped>
-  .logo {
-    text-align: center;
-    width: 100%;
-  }
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+min-width: 100%;
+min-height: 100%;
+}
+
+.logo {
+  text-align: center;
+  width: 100%;
+}
 </style>
