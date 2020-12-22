eb25d5fde17766cb1243d045e3c43a8488420cd2
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-12-22 11:11:37 +0100

    Add category

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
     <b-col col md="6">
         <div>
-            <h6 class="subheading">Aufgabe</h6>
+            <h6 class="subheading">{{task.category}}</h6>
             <h1 class="heading">{{task.name}}</h1>
             <p>{{task.description}}</p>
         </div>
