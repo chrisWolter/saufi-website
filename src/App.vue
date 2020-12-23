901904fe61db5f39ae850f316b0551ce7f7ca969
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-12-23 13:00:13 +0100

    Update shadow, remove fonts

--- src/App.vue
+++ src/App.vue
@@ -132,7 +132,7 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Kalam:wght@400;700&display=swap');
   html, body {
     margin: 0;
@@ -151,7 +151,7 @@ export default {
     font-weight: 700;
   }
   .decider-elem  {
-    box-shadow: 0px 0px 5px rgba(0,0,0,0.1);
+    box-shadow: 0px 0px 5px rgba(0,0,0,0.2);
     border-radius: 1.7em;
     padding: 1em;
     margin: 15px 25px;
