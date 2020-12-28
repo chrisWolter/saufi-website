661512fbbcda785ce101057ff2423adaa8a163fb
Alex <alexander.daniel.herrmann@gmx.de>
2020-12-28 23:20:46 +0100

    New favicon + edited modal content

--- src/components/ModalContent.vue
+++ src/components/ModalContent.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="d-block">
       <h4>Das Spiel</h4>
-      <p>Ihr wollt ein Trinkspiel, das mal etwas anders ist? hier bekommt ihr es. Bei dem Spiel "Hab ich Saufi gehört"
-        wird das Handy herumgereicht und bekommt angezeigt ob man Saufi gehört hat, in dem man auf den Knopf am Ende der Seite drauf drückt.
-        Wenn Ihr Saufi gehört habt, dann müsst ihr etwas trinken. Was Ihr trinkt ist uns egal. Wieviel Ihr trinkt könnt ihr bevor ihr anfangt selber festlegen.
-        Es kann vorkommen das einem zusätzlich noch Aufgaben angezeigt wird. Diese solltet Ihr zusätzlich zum trinken oder nicht trinken erledigen.
+      <p>Ihr wollt ein Trinkspiel und seid zu faul euch was eigenes Einfallen zu lassen ? Say no more. Hier bekommt ihr es: Bei dem Spiel "Hab ich Saufi gehört"
+        wird das Handy herumgereicht und ihr bekommt angezeigt, ob ihr Saufi gehört habt oder ebend nicht. In dem man auf den Knopf am unteren Ende der Seite drückt, werden
+        die Würfel neu gerollt und der nächste Spieler hat die möglich seiner Leber zu schaden. Wenn Ihr Saufi gehört habt, dann müsst ihr etwas trinken. Was Ihr trinkt ist uns egal. Wieviel Ihr trinkt könnt ihr bevor ihr anfangt selber festlegen.
+        Es kann vorkommen das euch zusätzlich noch Aufgaben angezeigt werden. Diese solltet Ihr zusätzlich zum trinken oder nicht trinken erledigen.
       </p>
       <h4>Allgemeine Infos</h4>
       <p>Diese Website wurde mit viel Herz und Alkohol von Alexander Herrmann
-        und Christian Wolter entwickelt. Bei Fragen, Anregungen oder Ideen für Spiele könnt Ihr uns einfach eine Emails schreiben.</p>
+        und Christian Wolter entwickelt. Bei Fragen, Anregungen oder Ideen für Spiele könnt Ihr uns einfach eine Email schreiben.</p>
         <a href="mailto:alex@hab-ich-saufi-gehoert.de">Mail an Alex</a>
         <br />
         <a href="mailto:chris@hab-ich-saufi-gehoert.de" class="mt-4">Mail an Chris</a>
       <h4 class="mt-4">Roadmap</h4>
-      <p>Wir werden dieses Trinkspiel weiterentwickeln und neue Ideen einfließen lassen. Bis ein Update kommt, wird es aber noch ein bisschen gehen.
-        Bugs und anpassungen werden aber regelmäßig geupdated.
+      <p>Wir werden dieses Trinkspiel weiterentwickeln und neue Ideen einfließen lassen. Allerdings kommen Major Updates unregelmäßig.
+        Bugfixes und kleinere Anpassungen werden wir aber nachbessern.
       </p>
-      <p class="mt-4">Aktuelle Version: 1.0.0</p>
+      <p class="mt-4">Aktuelle Version: 1.8.7</p>
     </div>
 </template>
+<script>
+  export default {
+    name: 'ModalContent'
+  }
+</script>
