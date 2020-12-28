d28c738559d4a411049674e5cf114d92f5f28967
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-12-28 23:33:57 +0100

    Update Spelling

--- src/components/ModalContent.vue
+++ src/components/ModalContent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="d-block">
       <h4>Das Spiel</h4>
-      <p>Ihr wollt ein Trinkspiel und seid zu faul euch was eigenes Einfallen zu lassen ? Say no more. Hier bekommt ihr es: Bei dem Spiel "Hab ich Saufi gehört"
+      <p>Ihr wollt ein Trinkspiel und seid zu faul euch was eigenes einfallen zu lassen? Say no more. Hier bekommt ihr es: Bei dem Spiel "Hab ich Saufi gehört"
         wird das Handy herumgereicht und ihr bekommt angezeigt, ob ihr Saufi gehört habt oder ebend nicht. In dem man auf den Knopf am unteren Ende der Seite drückt, werden
-        die Würfel neu gerollt und der nächste Spieler hat die möglich seiner Leber zu schaden. Wenn Ihr Saufi gehört habt, dann müsst ihr etwas trinken. Was Ihr trinkt ist uns egal. Wieviel Ihr trinkt könnt ihr bevor ihr anfangt selber festlegen.
+        die Würfel neu gerollt und der nächste Spieler hat die möglich seiner Leber zu schaden oder eben nicht. Wenn Ihr Saufi gehört habt, dann müsst ihr etwas trinken. Was Ihr trinkt ist uns egal. Wieviel Ihr trinkt könnt ihr bevor ihr anfangt selber festlegen.
         Es kann vorkommen das euch zusätzlich noch Aufgaben angezeigt werden. Diese solltet Ihr zusätzlich zum trinken oder nicht trinken erledigen.
       </p>
       <h4>Allgemeine Infos</h4>
