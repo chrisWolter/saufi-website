d2a255aac70150b4956ce1d26798b1de3b6da821
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-12-28 14:32:00 +0100

    outsourced modal content

--- src/App.vue
+++ src/App.vue
@@ -26,27 +26,8 @@
         </b-row>
       </div>
     </b-container>
-
       <b-modal id="impressum-modal" title="Information" hide-footer>
-    <div class="d-block">
-      <h4>Das Spiel</h4>
-      <p>Ihr wollt ein Trinkspiel, das mal etwas anders ist? hier bekommt ihr es. Bei dem Spiel "Hab ich Saufi gehört"
-        wird das Handy herumgereicht und bekommt angezeigt ob man Saufi gehört hat, in dem man auf den Knopf am Ende der Seite drauf drückt.
-        Wenn Ihr Saufi gehört habt, dann müsst ihr etwas trinken. Was Ihr trinkt ist uns egal. Wieviel Ihr trinkt könnt ihr bevor ihr anfangt selber festlegen.
-        Es kann vorkommen das einem zusätzlich noch Aufgaben angezeigt wird. Diese solltet Ihr zusätzlich zum trinken oder nicht trinken erledigen.
-      </p>
-      <h4>Allgemeine Infos</h4>
-      <p>Diese Website wurde mit viel Herz und Alkohol von Alexander Herrmann
-        und Christian Wolter entwickelt. Bei Fragen, Anregungen oder Ideen für Spiele könnt Ihr uns einfach eine Emails schreiben.</p>
-        <a href="mailto:alex@hab-ich-saufi-gehoert.de">Mail an Alex</a>
-        <br />
-        <a href="mailto:chris@hab-ich-saufi-gehoert.de" class="mt-4">Mail an Chris</a>
-      <h4 class="mt-4">Roadmap</h4>
-      <p>Wir werden dieses Trinkspiel weiterentwickeln und neue Ideen einfließen lassen. Bis ein Update kommt, wird es aber noch ein bisschen gehen.
-        Bugs und anpassungen werden aber regelmäßig geupdated.
-      </p>
-      <p class="mt-4">Aktuelle Version: 1.0.0</p>
-    </div>
+    <modal-content></modal-content>
   </b-modal>
   </div>
 </template>
@@ -54,12 +35,14 @@
 <script>
 import Decider from './components/Decider.vue'
 import Task from './components/Task.vue'
+import ModalContent from '@/components/ModalContent'
 
 export default {
   name: 'App',
   components: {
     Decider,
-    Task
+    Task,
+    ModalContent
   },
   data () {
     return {
