f47a468afd1cb8c30319b377e0487e937da3dabc
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2021-06-10 13:09:45 +0200

    Implemented new toggle

--- src/components/ToggleDarkModeButton.vue
+++ src/components/ToggleDarkModeButton.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="d-flex toggle-wrapper">
     <label for="DarkmodeSwitch" class="mb-0">Darkmode</label>
-    <b-form-checkbox v-model="isDarkMode" @change="toggleDarkMode" id="DarkmodeSwitch" name="DarkmodeSwitch" switch size="lg" class="ml-auto toggle">
-    </b-form-checkbox>
+<label class="switch ml-auto toggle">
+  <input type="checkbox" v-model="isDarkMode" @change="toggleDarkMode" checked>
+  <span class="slider round"></span>
+</label>
+    <!-- <b-form-checkbox v-model="isDarkMode" @change="toggleDarkMode" id="DarkmodeSwitch" name="DarkmodeSwitch" switch size="lg" class="ml-auto toggle">
+    </b-form-checkbox> -->
   </div>
 </template>
 <script>
@@ -47,5 +51,63 @@ export default {
   }
   .toggle{
     align-self: center;
+    margin-top: 6px;
   }
+
+  .switch {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 24px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(16px);
+  -ms-transform: translateX(16px);
+  transform: translateX(16px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
 </style>
