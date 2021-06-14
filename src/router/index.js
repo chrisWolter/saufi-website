77c2fd37aac6b5ee5b42219d8ef6ad5769c96741
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2021-06-14 18:37:33 +0200

    added new router links

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home'
 import Explanation from '@/views/Explanation'
+import AboutUs from '@/views/AboutUs'
+import Changelog from '@/views/Changelog'
 
 Vue.use(VueRouter)
 
@@ -12,9 +14,19 @@ const routes = [
       component: Home
     },
     {
-      path: '/the-game',
+      path: '/das-spiel',
       name: 'Explanation',
       component: Explanation
+    },
+    {
+      path: '/about-us',
+      name: 'AboutUs',
+      component: AboutUs
+    },
+    {
+      path: '/changelog',
+      name: 'Changelog',
+      component: Changelog
     },
 ]
 
